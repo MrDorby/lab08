b1712a1f6616f2613c4a9f3361bfbdc2bde5cc0e
Simone Carpi - simone.carpi@studio.unibo.it <simone.carpi@studio.unibo.it>
2023-11-13 13:23:04 +0100

    Add methods to TestDeathNote

--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,33 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+
+class TestDeathNote {
+    private DeathNote death;
+   @BeforeEach
+   public void setUp(){
+    this.death = new DeathNoteImpl();
+   }
+
+   @Test
+   public void validRange(){
+    try{
+        death.getRule(0);
+        fail("Error: expected IllegalArgumentException");
+    }catch(IllegalArgumentException e){
+        assertNotNull(e, e.toString());
+        assertEquals(0, e.toString().length());
+    }
+
+    try {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+   }
 }
