c8e09694ad9ca7be3053bfb4a0852d3d1067ca32
Simone <simone.carpi@studio.unibo.it>
2023-11-15 18:46:44 +0100

    Completed the Class TestDeathNote.java with the implementation of the methods verifying that all methods fail

--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -2,6 +2,8 @@ package it.unibo.deathnote;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.Thread;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,25 +11,75 @@ import it.unibo.deathnote.api.DeathNote;
 
 class TestDeathNote {
     private DeathNote death;
-   @BeforeEach
-   public void setUp(){
-    this.death = new DeathNoteImpl();
-   }
 
-   @Test
-   public void validRange(){
-    try{
-        death.getRule(0);
-        fail("Error: expected IllegalArgumentException");
-    }catch(IllegalArgumentException e){
-        assertNotNull(e, e.toString());
-        assertEquals(0, e.toString().length());
+    @BeforeEach
+    public void setUp(){
+        this.death = new DeathNoteImpl();
     }
 
-    try {
-        
-    } catch (Exception e) {
-        // TODO: handle exception
+    @Test
+    public void validRange(){
+        try{
+            death.getRule(0);
+            fail("Error: expected IllegalArgumentException");
+        }catch(IllegalArgumentException e){
+            assertNotNull(e, e.toString());
+            assertEquals(0, e.toString().length());
+        }
     }
-   }
+
+    @Test
+    public void validRules(){
+        for(String s: DeathNote.RULES){
+            assertNotNull(s);
+        }
+    }
+
+    @Test
+    public void nameWritten(){
+        assertFalse(death.isNameWritten("Pippo"));
+        death.writeName("Pippo");
+        assertTrue(death.isNameWritten("Pippo"));
+        assertFalse(death.isNameWritten("Pluppo"));
+        assertFalse(death.isNameWritten(""));
+    }
+
+    @Test
+    public void publicDeathCause(){
+        try {
+            death.writeDeathCause("");
+            fail("Error: expected IllegalStateException");
+        } catch (IllegalStateException e) {
+            death.writeName("Pippo");
+            assertEquals(death.getDeathCause("Pippo"), "heart attack");
+            death.writeName("Pluppo");
+            death.writeDeathCause("karting accident");
+            try{
+                Thread.sleep(100);
+            }catch(InterruptedException m){
+                death.writeDeathCause("RIP");
+                assertEquals(death.getDeathCause("Pluppo"), "RIP");
+            }
+        }
+    }
+
+    @Test
+    public void publicDeathDetail(){
+        try {
+            death.writeDetails("");
+            fail("Error: expected IllegalStateException");
+        } catch (IllegalStateException e) {
+            death.writeName("Pippo");
+            assertEquals(death.getDeathDetails("Pippo"), "");
+            death.writeDetails("ran for too long");
+            death.writeName("Pluppo");
+            try{
+                Thread.sleep(6100);
+            }catch(InterruptedException m){
+                death.writeDetails("RIP");
+                assertEquals(death.getDeathDetails("Pluppo"), "RIP");
+            }
+        }
+    }
+
 }
